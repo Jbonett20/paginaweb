896e808eb9296b1e945d2b50fc332cd307356e46
Jbonett <jbn19860220@gmail.com>
2025-04-23 18:07:08 -0500

    arreglos verison 3

--- modulos/team.php
+++ modulos/team.php
@@ -2,7 +2,7 @@
 
   <!-- Section Title -->
   <div class="container section-title" data-aos="fade-up">
-    <h2 class="text-center text-uppercase">Equipo</h2>
+    <h2 class="text-align text-uppercase">Equipo</h2>
   </div>
 
   <div class="container">
@@ -19,17 +19,17 @@
       <!-- Miembro 1 -->
       <div class="col-xl-4 col-lg-5 col-md-6" data-aos="fade-up" data-aos-delay="300">
         <div class="member text-center">
-          <img src="assets/img/team/team-3.jpg" class="img-fluid rounded" alt="Claudia Marcela Gonzales">
+          <img src="assets/img/team/team-3.png" class="img-fluid rounded" alt="Claudia Marcela Gonzales">
           <div class="member-info mt-3">
             <div class="member-info-content text-white">
               <h4>Claudia Marcela Gonzales</h4>
               <span>Socia Fundadora</span>
             </div>
             <div class="social mt-2">
-              <a href="#"><i class="bi bi-twitter-x"></i></a>
-              <a href="#"><i class="bi bi-facebook"></i></a>
+              <!-- <a href="#"><i class="bi bi-twitter-x"></i></a>
+              <a href="#"><i class="bi bi-facebook"></i></a> -->
               <a href="#"><i class="bi bi-instagram"></i></a>
-              <a href="#"><i class="bi bi-linkedin"></i></a>
+              <a href="https://www.linkedin.com/in/claudia-marcela-gonzalez-martinez-558549121?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"><i class="bi bi-linkedin"></i></a>
             </div>
           </div>
         </div>
@@ -38,15 +38,15 @@
       <!-- Miembro 2 -->
       <div class="col-xl-4 col-lg-5 col-md-6" data-aos="fade-up" data-aos-delay="400">
         <div class="member text-center">
-          <img src="assets/img/team/team-4.jpg" class="img-fluid rounded" alt="Ana Paola Ballesteros">
+          <img src="assets/img/team/team-4.png" class="img-fluid rounded" alt="Ana Paola Ballesteros">
           <div class="member-info mt-3">
             <div class="member-info-content text-white">
               <h4>Ana Paola Ballesteros</h4>
               <span>Socia Fundadora</span>
             </div>
             <div class="social mt-2">
-              <a href="#"><i class="bi bi-twitter-x"></i></a>
-              <a href="#"><i class="bi bi-facebook"></i></a>
+             <!--  <a href="#"><i class="bi bi-twitter-x"></i></a>
+              <a href="#"><i class="bi bi-facebook"></i></a> -->
               <a href="#"><i class="bi bi-instagram"></i></a>
               <a href="#"><i class="bi bi-linkedin"></i></a>
             </div>
